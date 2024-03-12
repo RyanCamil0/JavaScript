ff88f3127025d199e806677155a76ad62996b655
Camilo <143614389+RyanCamil0@users.noreply.github.com>
2024-03-12 14:35:33 -0300

    fodase

---
+++ JavaScript/type-number.js
@@ -0,0 +1,27 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const  segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero *
+segundoNumero;
+
+// console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoflutuante = 3.3;
+const pontoFlutuanteSemzero = .5;
+
+const novaOperação = primeiroNumero / 
+numeroPontoflutuante;
+
+console.log(novaOperação)
+
+// NaN - Not A Number
+
+const alura = "Alura"
+
+console.log(alura + primeir)
